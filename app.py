5976b08e0b92c98b8c227a851b46ee3e51f4826f
dennerguti <dennerayres@gmail.com>
2025-04-01 15:20:10 -0300

    ✨Comunicação com API

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
-print("Hello world")
+import requests
+
+def pesquisar_especie(nome, limite=10):
+    url = "https://api.gbif.org/v1/occurrence/search"
+    params = {
+        "scientificName": nome,
+        "hasCoordinate": "true",
+        "limit": limite
+    }
+    
+    response = requests.get(url, params=params)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return data["results"]
+    
+    else:
+        print(f"Erro na requisição: {response.status_code}")
+        return []
+
+# Exemplo de uso
+test_species = "Chelonia mydas" 
+results = pesquisar_especie(test_species)
+print(results[:3])  
