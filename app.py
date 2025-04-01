e67c46203a6aa76a192121cea41f80e0e23a72ac
dennerguti <dennerayres@gmail.com>
2025-04-01 15:59:26 -0300

    ✨ Adicionar filtros

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import requests
 
-def pesquisar_especie(nome, limite=10):
+def pesquisar_especie(nome_cientifico, lat_min, lat_max, lon_min, lon_max, ano_inicio, ano_fim, limite=10):
     url = "https://api.gbif.org/v1/occurrence/search"
     params = {
-        "scientificName": nome,
+        "scientificName": nome_cientifico,
         "hasCoordinate": "true",
-        "limit": limite
+        "limit": limite,
+        "decimalLatitude": f"{lat_min},{lat_max}",
+        "decimalLongitude": f"{lon_min},{lon_max}",
+        "year": f"{ano_inicio},{ano_fim}"
     }
     
     response = requests.get(url, params=params)
@@ -18,7 +21,6 @@ def pesquisar_especie(nome, limite=10):
         print(f"Erro na requisição: {response.status_code}")
         return []
 
-# Exemplo de uso
-test_species = "Chelonia mydas" 
-results = pesquisar_especie(test_species)
-print(results[:3])  
+especie = "Asterias rubens"  
+resultado = pesquisar_especie(especie, -30, 30, -90, -60, 2000, 2020)
+print(resultado[:3])  
